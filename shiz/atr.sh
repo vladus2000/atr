754c4b8e5c28996a18f2b1eb66d397a161fc2607
vladus2000 <docker@matt.land>
2020-08-11 07:07:03 -0500

    fixing startup

--- shiz/atr.sh
+++ shiz/atr.sh
@@ -1,2 +1,3 @@
 cd /downloads
-{ cat config; tail -f /dev/null; } | python /home/evil/atr/main.py > out 2> out2
+#{ cat config; tail -f /dev/null; } | python /home/evil/atr/main.py > out 2> out2
+{ cat config; tail -f /dev/null; } | python /home/evil/atr/atr_cmd.py > out 2> out2
