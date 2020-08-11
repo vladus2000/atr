4a2b3264bafa34fef71860e59922bf8dc8f11167
vladus2000 <docker@matt.land>
2020-08-11 14:43:51 -0500

    going back to websocket branch

--- shiz/atr.sh
+++ shiz/atr.sh
@@ -1,3 +1,2 @@
 cd /downloads
-#{ cat config; tail -f /dev/null; } | python /home/evil/atr/main.py > out 2> out2
-{ cat config; tail -f /dev/null; } | python /home/evil/atr/atr_cmd.py > out 2> out2
+{ cat config; tail -f /dev/null; } | python /home/evil/atr/main.py > out 2> out2
